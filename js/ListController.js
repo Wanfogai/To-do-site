e5f29ea7bf17bb6c72b1194a2c30dada51c0a061
Wanfogai <81505429+Wanfogai@users.noreply.github.com>
2024-10-22 23:12:45 +0300

    Загрузка проекта

---
+++ js/ListController.js
@@ -0,0 +1,79 @@
+
+window.onload = function () {
+    setTimeout(AddPage(), 0);
+}
+
+function GetContent(num) {
+    return document.getElementsByClassName('page')[parseInt(num)].getElementsByClassName("content")[0];
+}
+
+function RemoveChild(control, childNum) {
+    control.removeChild(control.children[childNum])
+    for (let index = 0; index < control.children.length; index++) {
+        control.children[index].id = index;
+
+    }
+}
+
+function AddItem(event, parentNum) {
+
+    const item = document.createElement("div");
+    item.className = "item";
+
+    const items = GetContent(parentNum - 1).children.length - 1;
+    item.id = items + 1
+
+    const description = document.createElement("span");
+    description.className = "string";
+    description.onclick = (event) => { event.target.contentEditable = true };
+    description.onblur = (event) => { event.target.contentEditable = false };
+
+    description.textContent = "Пункт " + (items + 1);
+
+    const dellButt = document.createElement("spab");
+    dellButt.textContent = "X";
+    dellButt.className="dellButt";
+    dellButt.onclick = () => RemoveChild(GetContent(parentNum - 1), item.id);
+
+    const check = document.createElement("input");
+    check.type = "checkbox";
+
+    item.appendChild(description);
+    item.appendChild(check);
+    item.appendChild(dellButt);
+
+    GetContent(parentNum - 1).children[items].after(item);
+}
+
+function AddPage(event) {
+
+    const pages = document.getElementsByClassName("page");
+    const newPage = document.createElement("div");
+    newPage.className = "page";
+    newPage.id = pages.length + 1;
+    const pageTitle = document.createElement("div");
+    pageTitle.id = "page-title";
+    pageTitle.onclick = (event) => { event.target.contentEditable = true };
+    pageTitle.onblur = (event) => { event.target.contentEditable = false };
+    pageTitle.textContent = "Список " + newPage.id;
+
+    const content = document.createElement("div");
+    content.className = "content";
+
+
+    const newItem = document.createElement("div");
+    newItem.id = 0;
+    newItem.ondblclick = (event) => AddItem(event, newPage.id);
+    newItem.className = "new-item";
+    newItem.textContent = "....";
+
+
+    content.appendChild(newItem)
+
+    newPage.appendChild(pageTitle);
+    newPage.appendChild(content);
+    document.body.insertBefore(
+        newPage,
+        document.getElementById(-pages.length)
+    ); AddItem(event, newPage.id)
+}
