2b89ac60a35d48fb0f187741573b41544092bf83
Wanfogai <wanfogai770@gmail.com>
2024-10-31 16:16:24 +0200

    Обновление логики

--- js/ListController.js
+++ js/ListController.js
@@ -1,75 +1,37 @@
-
 window.onload = function () {
-    setTimeout(AddPage(), 0);
-}
+  setTimeout(AddPage(), 0);
+};
 
-function ReIndex(control) {
-    for (let index = 0; index < control.children.length; index++) {
-        control.children[index].id = index;
-    }
-}
-
-function RemoveChild(control, childNum) {
-    control.removeChild(control.children[childNum])
-    ReIndex(control);
-}
-
-function ToDown(control, itemId) {
-    control.appendChild(control.getElementsByClassName("item")[itemId - 1]);
-    ReIndex(control);
-}
-
-function GetContent(num) {
-    return document.getElementsByClassName('page')[parseInt(num)].getElementsByClassName("content")[0];
-}
-
-function AddItem(event, parentNum) {
-    const items = GetContent(parentNum - 1).children.length - 1;
-
-    const item = document.createElement("div");
-    item.className = "item";
-    item.id = items + 1
-
-    const description = document.createElement("span");
-    description.className = "string";
-    description.onclick = (event) => { event.target.contentEditable = true };
-    description.onblur = (event) => { event.target.contentEditable = false };
-    description.textContent = "Пункт " + (items + 1);
-
-    const actionContainer = document.createElement("div");
-    actionContainer.className = "actionContainer";
-
-    const dellButt = document.createElement("spab");
-    dellButt.textContent = "X";
-    dellButt.className = "itemButt";
-    dellButt.onclick = () => RemoveChild(GetContent(parentNum - 1), item.id);
-
-    const check = document.createElement("input");
-    check.type = "checkbox";
-    check.onchange = () => { if (check.checked) ToDown(GetContent(parentNum - 1), item.id); };
-
-    actionContainer.appendChild(dellButt);
-    actionContainer.appendChild(check);
-
-    item.appendChild(description);
-    item.appendChild(actionContainer);
-
-    GetContent(parentNum - 1).children[items].after(item);
-}
+Pages = [];
 
 function AddPage(event) {
+  const NewPage = new Page(Pages.length);
 
-    const pages = document.getElementsByClassName("page");
+  NewPage.AddItem();
+  Pages.unshift(NewPage);
 
+  if (Pages.length <= 1) document.body.appendChild(NewPage.Node);
+  else document.body.insertBefore(NewPage.Node, Pages[1].Node);
+}
+
+class Page {
+  Node;
+  CurrentContent;
+  Items = [];
+
+  constructor(PageCount) {
     const newPage = document.createElement("div");
-    newPage.id = pages.length + 1;
-    newPage.className = "page page-" + (pages.length + 1);
+    newPage.className = "page page-" + (PageCount + 1);
 
     const pageTitle = document.createElement("div");
     pageTitle.id = "page-title";
-    pageTitle.onclick = (event) => { event.target.contentEditable = true };
-    pageTitle.onblur = (event) => { event.target.contentEditable = false };
-    pageTitle.textContent = "Список " + newPage.id;
+    pageTitle.onclick = (event) => {
+      event.target.contentEditable = true;
+    };
+    pageTitle.onblur = (event) => {
+      event.target.contentEditable = false;
+    };
+    pageTitle.textContent = "Список " + (PageCount + 1);
 
     const header = document.createElement("div");
     header.className = "header";
@@ -77,18 +39,18 @@ function AddPage(event) {
     const dellButt = document.createElement("span");
     dellButt.textContent = "X";
     dellButt.className = "pageButt";
-    dellButt.onclick = () => document.body.removeChild(document.getElementsByClassName("page-" + newPage.id)[0])
+    dellButt.onclick = () => document.body.removeChild(this.Node);
 
     const content = document.createElement("div");
     content.className = "content";
 
     const newItem = document.createElement("div");
     newItem.id = 0;
-    newItem.ondblclick = (event) => AddItem(event, newPage.id);
+    newItem.onclick = (event) => this.AddItem(event, newPage.id);
     newItem.className = "new-item";
-    newItem.textContent = "....";
+    newItem.textContent = "Добавить";
 
-    content.appendChild(newItem)
+    content.appendChild(newItem);
 
     header.appendChild(pageTitle);
     header.appendChild(dellButt);
@@ -96,9 +58,53 @@ function AddPage(event) {
     newPage.appendChild(header);
     newPage.appendChild(content);
 
-    document.body.insertBefore(
-        newPage,
-        document.getElementById(-pages.length)
-    );
-    AddItem(event, newPage.id)
+    this.Node = newPage;
+    this.CurrentContent = content;
+  }
+
+  AddItem() {
+    const NewItem = new Item(this.Items.length, this.CurrentContent);
+    this.CurrentContent.lastChild.after(NewItem.Node);
+    this.Items.push(NewItem);
+  }
+}
+
+class Item {
+  Node;
+  Parent;
+
+  constructor(itemCount, parent) {
+    const item = document.createElement("div");
+    item.className = "item";
+
+    const description = document.createElement("span");
+    description.className = "string";
+    description.onclick = (event) => {
+      event.target.contentEditable = true;
+    };
+    description.onblur = (event) => {
+      event.target.contentEditable = false;
+    };
+    description.textContent = "Пункт " + (itemCount + 1);
+
+    const actionContainer = document.createElement("div");
+    actionContainer.className = "actionContainer";
+
+    const dellButt = document.createElement("spab");
+    dellButt.textContent = "X";
+    dellButt.className = "itemButt";
+    dellButt.onclick = () => this.Parent.removeChild(this.Node);
+
+    const check = document.createElement("input");
+    check.type = "checkbox";
+
+    actionContainer.appendChild(dellButt);
+    actionContainer.appendChild(check);
+
+    item.appendChild(description);
+    item.appendChild(actionContainer);
+
+    this.Node = item;
+    this.Parent = parent;
+  }
 }
